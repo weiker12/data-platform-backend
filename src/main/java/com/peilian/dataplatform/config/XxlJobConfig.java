8c090250c10d66561390cb9f4edc40cec945eebd
zhengshangchao <zhengshangchao@peilian.com>
2020-08-04 13:29:35 +0800

    增加注释

--- src/main/java/com/peilian/dataplatform/config/XxlJobConfig.java
+++ src/main/java/com/peilian/dataplatform/config/XxlJobConfig.java
@@ -5,6 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * xxl-job配置
+ *
+ * @author zhengshangchao
+ */
 @Configuration
 public class XxlJobConfig {
 
