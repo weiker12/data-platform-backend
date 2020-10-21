4c73c1a1a2c0029097b6d562267a35083a89ceda
zhengshangchao <zhengshangchao@peilian.com>
2020-10-21 11:00:15 +0800

    增加缓存map进行性能优化

--- src/main/java/com/peilian/dataplatform/config/MysqlFactory.java
+++ src/main/java/com/peilian/dataplatform/config/MysqlFactory.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * mysql工厂
  *
@@ -14,8 +18,11 @@ import org.springframework.util.Assert;
 @Component
 public class MysqlFactory {
 
-    @Autowired
-    DataSourceRepository dataSourceRepository;
+    /**
+     * 以dsCode为键的缓存MysqlConnector，避免频繁创建MysqlConnector产生开销
+     */
+    private final static Map<String, MysqlConnector> cacheMap= new HashMap<>();
+
 
     /**
      * 创建连接mysql实例的工厂
@@ -25,10 +32,15 @@ public class MysqlFactory {
      */
     public MysqlConnector build(DataSource dataSource) {
         Assert.notNull(dataSource, "数据源配置不能为空！");
+        String dsCode = dataSource.getDsCode();
         String url = dataSource.getUrl();
         String username = dataSource.getUsername();
         String password = dataSource.getPassword();
-        MysqlConnector mysqlConnector = new MysqlConnector(url, username, password);
+        MysqlConnector mysqlConnector = cacheMap.get(dsCode);
+        if(Objects.isNull(mysqlConnector)) {
+            mysqlConnector = new MysqlConnector(url, username, password);
+            cacheMap.put(dsCode, mysqlConnector);
+        }
         return mysqlConnector;
     }
 
